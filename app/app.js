4b52bb319bfd393f5457758a44733d7b1e29605b
javierplacencio <javier.placencio@hiq.se>
2019-07-05 00:06:53 +0200

    frontend working with real rate values for EUR, which is still hardcoded on the front end

--- app/app.js
+++ app/app.js
@@ -4,16 +4,17 @@ const APIRouter = require('./api-app')
 
 const DATA_URL = 'http://data.fixer.io/api/latest?access_key=45bfc1f0f754a56d0c3477f0fce6c074' // TODO: create config file with API URL and KEY 45bfc1f0f754a56d0c3477f0fce6c074
 
-app.get('/app', (req, res) => res.sendFile(__dirname + '/public/index.html')) // ANGULAR APP
 
 app.use('/api', new APIRouter(DATA_URL).getRouter()) // API paths
 
-app.use("*", (req, res) => res.redirect('/app')); // fallbacks by redirecting to web app
+app.get('/app', (req, res) => res.sendFile(__dirname + '/public/index.html')) // ANGULAR APP
+app.use(express.static(__dirname + '/public'));
 
 app.use((err, req, res, next) => {
 	console.error(err.stack)
 	res.status(500)
 })
+
 
 app.listen(8080)
 
--- app/public/js/index.js
+++ app/public/js/index.js
@@ -2,12 +2,33 @@
 Contains all logic for the web app which is not much. Not worth having separate files at this point.
 */
 'use strict'
-angular.module('CurrencyApp', [])
-    .service('ApiService', ['$http', function ($http) {
-    // TODO: implement communication with server
-    }])
-    .controller
-  .controller('MainController', ['$scope', function ($scope) {
-    // TODO: implement interactions with user
+angular
+    .module('CurrencyApp', [])
+    .controller('MainController', ['$scope', 'ApiService', function ($scope, ApiService) {
+        $scope.currencyToList = []
+        $scope.currencyBase = Object.assign([], ApiService.currencyBaseList).pop() // extract any currency to start with
 
-}])
+        ApiService.fetchRates($scope.currencyBase)
+            .success(function(response) {
+                console.log(response)
+                $scope.rates = response.rates
+                $scope.currencyToList = Object.keys($scope.rates)
+            })
+
+        $scope.updateResult = function() {
+            var rate = $scope.rates[$scope.currencyToSelected]
+            $scope.resultAmount = $scope.currencyAmount * rate
+        }
+    }])
+    .factory('ApiService', ['$http', function ($http) {
+        var service = this
+
+        service.currencyBaseList = ['EUR']; // TODO: fetch from server
+
+        service.fetchRates = function(currency) {
+            var url = '/api/currency?base=' + currency
+            return $http.get(url)
+        }
+
+        return service;
+    }])
