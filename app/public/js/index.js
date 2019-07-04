51c755786fc1ab3b811cea8437c9f85f9a0e80c4
javierplacencio <javier.placencio@hiq.se>
2019-07-05 00:40:43 +0200

    fetching all data from server and handling errors on web app (license error basically)

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -5,17 +5,49 @@ Contains all logic for the web app which is not much. Not worth having separate 
 angular
     .module('CurrencyApp', [])
     .controller('MainController', ['$scope', 'ApiService', function ($scope, ApiService) {
+        $scope.currencyBaseList = []
         $scope.currencyToList = []
-        $scope.currencyBase = Object.assign([], ApiService.currencyBaseList).pop() // extract any currency to start with
 
-        ApiService.fetchRates($scope.currencyBase)
-            .success(function(response) {
-                console.log(response)
-                $scope.rates = response.rates
-                $scope.currencyToList = Object.keys($scope.rates)
-            })
+        init()
 
-        $scope.updateResult = function() {
+        /** EXPOSED METHODS **/
+        $scope.fetchRates = fetchRates
+        $scope.updateResult = updateResult
+
+        /** METHODS **/
+
+        function init() {
+            ApiService.fetchCurrencies()
+                .success(function(response) {
+                    console.log('fetchCurrencies', response)
+                    $scope.currencyBaseList = Object.keys(response.rates)
+                    if ($scope.currencyBaseList.includes('EUR')) { // pre-select EUR if available since is the only one with free license
+                        $scope.currencyBaseSelected = 'EUR'
+                    } else {
+                        $scope.currencyBaseSelected = Object.assign([], $scope.currencyBaseList).pop() // extract any currency to start with
+                    }
+
+                    fetchRates()
+                })
+                .catch(console.error)
+
+        }
+
+        function fetchRates() {
+            ApiService.fetchRates($scope.currencyBaseSelected)
+                .success(function(response) {
+                    $scope.errorMsg = undefined
+                    console.log('fetchRates', response)
+                    $scope.rates = response.rates
+                    $scope.currencyToList = Object.keys($scope.rates)
+                }).catch(function(e) {
+                    console.error(e)
+                    $scope.resultAmount = undefined
+                    $scope.errorMsg = e.data
+                })
+        }
+
+        function updateResult() {
             var rate = $scope.rates[$scope.currencyToSelected]
             $scope.resultAmount = $scope.currencyAmount * rate
         }
@@ -23,7 +55,14 @@ angular
     .factory('ApiService', ['$http', function ($http) {
         var service = this
 
-        service.currencyBaseList = ['EUR']; // TODO: fetch from server
+
+
+        /* METHODS */
+
+        service.fetchCurrencies = function() {
+            var url = '/api/currency?base=' + 'EUR'
+            return $http.get(url)
+        }
 
         service.fetchRates = function(currency) {
             var url = '/api/currency?base=' + currency
