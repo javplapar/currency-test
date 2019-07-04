db30317f056674447df9c6dd71fc4acef7fe7def
javierplacencio <javier.placencio@hiq.se>
2019-07-04 23:02:27 +0200

    introducing web app (static now)

--- app/app.js
+++ app/app.js
@@ -1,14 +1,19 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const APIRouter = require('./api-app')
 
 const DATA_URL = 'http://data.fixer.io/api/latest?access_key=45bfc1f0f754a56d0c3477f0fce6c074' // TODO: create config file with API URL and KEY 45bfc1f0f754a56d0c3477f0fce6c074
+
+app.get('/app', (req, res) => res.sendFile(__dirname + '/public/index.html')) // ANGULAR APP
+
+app.use('/api', new APIRouter(DATA_URL).getRouter()) // API paths
+
+app.use("*", (req, res) => res.redirect('/app')); // fallbacks by redirecting to web app
 
 app.use((err, req, res, next) => {
 	console.error(err.stack)
 	res.status(500)
 })
-
-app.use('/api', new APIRouter(DATA_URL).getRouter())
 
 app.listen(8080)
 
---
+++ app/public/js/index.js
@@ -0,0 +1,13 @@
+/**
+Contains all logic for the web app which is not much. Not worth having separate files at this point.
+*/
+'use strict'
+angular.module('CurrencyApp', [])
+    .service('ApiService', ['$http', function ($http) {
+    // TODO: implement communication with server
+    }])
+    .controller
+  .controller('MainController', ['$scope', function ($scope) {
+    // TODO: implement interactions with user
+
+}])
